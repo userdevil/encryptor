f494dfd89b6da984796c25e54dcb6cd74f668b05
userdevil <62639000+userdevil@users.noreply.github.com>
2022-02-27 13:32:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,5 +55,3 @@ def decode():
         message = message_bytes.decode('ascii')
         return render_template('index.html',msg2 = message)
     
-if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
